d6d396cd00b834ad1a2af2d214fd6c6faf170dc5
Huszan <mateuszjacenty1@gmail.com>
2023-04-14 12:40:05 +0200

    Add music player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MovingBackgroundComponent } from './components/moving-background/moving-background.component';
+import { MusicPlayerComponent } from './components/music-player/music-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovingBackgroundComponent
+    MovingBackgroundComponent,
+    MusicPlayerComponent
   ],
   imports: [
     BrowserModule
