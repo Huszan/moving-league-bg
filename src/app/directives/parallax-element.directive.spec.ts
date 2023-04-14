43186f2f9865a83402eb5c56aec5c2cf9c623fd3
Huszan <mateuszjacenty1@gmail.com>
2023-04-14 23:21:19 +0200

    Add parallax animation

---
+++ src/app/directives/parallax-element.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ParallaxElementDirective } from './parallax-element.directive';
+
+describe('ParallaxElementDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ParallaxElementDirective();
+    expect(directive).toBeTruthy();
+  });
+});
